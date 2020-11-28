423ba9c6e6fa7808cb49fd5da2296598c5cce49b
Darren Zhang <darrenzhang22@gmail.com>
2020-11-28 14:16:36 -0800

    Places Markers on the Map to show where user's friends are at

--- app/src/main/java/edu/ucla/darrenzhang/tracela/Maps.java
+++ app/src/main/java/edu/ucla/darrenzhang/tracela/Maps.java
@@ -1,21 +1,48 @@
 package edu.ucla.darrenzhang.tracela;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 //import android.widget.Toolbar;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Maps extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng myLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +55,9 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
 
     }
 
@@ -48,9 +78,82 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
         }
         mMap.setMyLocationEnabled(true);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        Log.d(".Maps", "current location: (" + latitude + ", " + longitude + ")");
+                        LatLng latlngCurrLoc = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlngCurrLoc));
+                    }
+                }
+            }
+        });
+        placeMarkersForFriends();
         // Add a marker in Sydney and move the camera
 //        LatLng sydney = new LatLng(-34, 151);
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void placeMarkersForFriends() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest userGetRequest = new JsonArrayRequest(Request.Method.GET, Constants.DATABASE_URL + "/friends?username=" + MainActivity.username + "&reverse=true", new JSONArray(),
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray userList) {
+                        for (int i = 0; i < userList.length(); i++) {
+                            JSONObject user = null;
+                            try {
+                                user = userList.getJSONObject(i);
+                                String username = user.getString("username_b");
+                                placeMarkerFor(username);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.d(".Maps","number of friends is "+userList.length());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(".Maps", error.toString());
+            }
+        });
+
+        queue.add(userGetRequest);
+    }
+
+    public void placeMarkerFor(String friendUsername) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest coordGetRequest = new JsonArrayRequest(Request.Method.GET, Constants.DATABASE_URL + "/coords?username=" + friendUsername+"&justLocation=true", new JSONArray(),
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray coordinates) {
+                       if (coordinates.length()>0) {
+                                try{
+                                    JSONObject coordinate = coordinates.getJSONObject(coordinates.length()-1);
+                                    double latitude = coordinate.getDouble("lat");
+                                    double longitude = coordinate.getDouble("lng");
+                                    LatLng friend = new LatLng(latitude, longitude);
+                                    mMap.addMarker(new MarkerOptions().position(friend).title(friendUsername));
+                                }catch (JSONException e){
+                                    Log.d(".Maps","error processing location data: "+e.toString());
+                                }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(".Maps", "Getting coordinates for " + friendUsername + ": " + error.toString());
+            }
+        });
     }
 }
